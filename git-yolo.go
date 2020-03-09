96caab7921a020d0c7b85c12c350e69757cc843e
Kedwin Chen <kedwinchen@users.noreply.github.com>
2020-03-09 06:09:56 -0700

    When your kernel is a bucket of chicken

--- git-yolo.go
+++ git-yolo.go
@@ -25,7 +25,7 @@ func ePrint(err error, msg string) {
 	logErrror(err)
 	if err != nil {
 		log.Println("ERROR START >>> ")
-		log.Println(fmt.Sprintf("ERROR: (%s)", msg))
+		log.Println(fmt.Sprintf("%s", msg))
 		log.Println("ERROR END <<<")
 	}
 }
