7a8c50c1fc921919e6f14508c88871fd3aa2a4a1
Kedwin Chen <18587327+kedwinchen@users.noreply.github.com>
2020-03-14 12:29:43 -0700

    Add core functionality (#2)
    * 20200306 dev Add watcher (from github.com/fsnotify/fsnotify)
    
    (the end is nigh for sane messages)
    
    * 20203006 dev Change watcher
    
    - Change watcher so that it ignores the `.git` directory
    
    * 202003009 dev It works!
    
    Now it works! Enter the chaos
    
    * Squash tests of the chaos mode (#1)
    
    It now works, but let's squash these commits so it doesn't look too crazy
    20200314 dev Add better readme instructions
    
    Co-authored-by: Kedwin Chen <kedwinchen@users.noreply.github.com>

--- README.md
+++ README.md
@@ -2,4 +2,34 @@
 
 You only live once.
 
-As they say, commit often.
+As they say, commit often, perfect later (I feel like I'm missing a part of the phrase. Oh well, it's probably nothing)...
+
+## Installation
+
+(if the install script is not available yet)
+
+1. Download the source code (or download the binary and skip to step 3)
+2. Compile using `go build git-yolo.go`
+3. Add the resulting `git-yolo` binary to your path
+4. Add an alias for `git yolo` in your [global] `.gitconfig`
+5. Copy the `.gityolo` directory to your home directory (wherever that is)
+
+## Usage
+
+In any git directory, run `git yolo`.
+This starts `git-yolo` as a daemon-like process (currently, at time of writing, it blocks)
+
+## Why
+
+Why not?
+
+Also, it gave me a reason to write a project in Go.
+
+## WARNING
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+I am not responsible for any consequences that may result from using this software,
+professional or otherwise.
---
+++ git-yolo.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/radovskyb/watcher"
+)
+
+func logErrror(err error) {
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func ePrint(err error, msg string) {
+	logErrror(err)
+	if err != nil {
+		log.Println("ERROR START >>> ")
+		log.Println(fmt.Sprintf("%s", msg))
+		log.Println("ERROR END <<<")
+	}
+}
+
+func exitFail(msg string) {
+	// currently a wrapper, may change functionality later
+	log.Fatalln(msg)
+}
+
+func exitOnError(err error, msg string) {
+	logErrror(err)
+	exitFail(msg)
+}
+
+func readFile(filepath string) []string {
+	fileLines := []string{}
+
+	file, err := os.Open(filepath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fileLines = append(fileLines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return fileLines
+}
+
+func getMessages() []string {
+	messageList := []string{}
+	msgFileList := []string{}
+
+	yoloDir, err := homedir.Dir()
+	logErrror(err)
+	yoloDir = filepath.Join(yoloDir, ".gityolo")
+
+	pathInfo, err := os.Stat(yoloDir)
+	// exitOnError(err, fmt.Sprintf("FATAL: Directory %s does not exist!", yoloDir))
+	if !os.FileMode.IsDir(pathInfo.Mode()) {
+		exitFail(fmt.Sprintf("FATAL: Expected %s to be a directory", yoloDir))
+	}
+	// get a list of all text files in yoloDir
+	err = filepath.Walk(yoloDir,
+		func(path string, info os.FileInfo, err error) error {
+			if err == nil {
+				if strings.HasSuffix(path, ".txt") {
+					pathInfo, err = os.Stat(path)
+					if os.FileMode.IsRegular(pathInfo.Mode()) {
+						msgFileList = append(msgFileList, path)
+					}
+				}
+			}
+			return err
+		})
+	logErrror(err)
+	for _, theFile := range msgFileList {
+		theContents := readFile(theFile)
+		for _, theLine := range theContents {
+			messageList = append(messageList, theLine)
+		}
+	}
+
+	return messageList
+}
+
+func pickMessage(messageList *[]string, r *rand.Rand) string {
+	return (*messageList)[r.Intn(len(*messageList))]
+}
+
+func runCmd(cmd *exec.Cmd) error {
+	output, err := cmd.CombinedOutput()
+	ePrint(err, string(output))
+	return err
+}
+
+func GitYolo(messageList *[]string, r *rand.Rand) {
+	// add gitignore ignored files
+	gitAdd := exec.Command("git", "add", ".", "--force")
+	// commit with random messag
+	gitCommit := exec.Command("git", "commit", "-m", pickMessage(messageList, r))
+	// (force) push to master. what could possibly go wrong?
+	gitPushForce := exec.Command("git", "push", "--force", "origin", "master")
+	gitPush := exec.Command("git", "push", "--force")
+
+	runCmd(gitAdd)
+	runCmd(gitCommit)
+	runCmd(gitPushForce)
+	runCmd(gitPush)
+}
+
+func runWatcher(messageList *[]string, r *rand.Rand) {
+	theWatcher := watcher.New()
+	defer theWatcher.Close()
+
+	go func() {
+		for {
+			select {
+			case event := <-theWatcher.Event:
+				log.Println(event)
+				GitYolo(messageList, r)
+			case err := <-theWatcher.Error:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	theWatcher.AddRecursive(".")
+	theWatcher.Ignore(".git")
+	if err := theWatcher.Start(time.Millisecond); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func main() {
+	messageList := getMessages()
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	runWatcher(&messageList, r)
+}
