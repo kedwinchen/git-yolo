2bf74b29be2a45929844e4a2be2267e801f58686
Kedwin Chen <kedwinchen@users.noreply.github.com>
2020-03-15 16:58:39 -0700

    20200315 feature/cli-options Lay framework for CLI options

--- git-yolo.go
+++ git-yolo.go
@@ -9,9 +9,12 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+
+	// "syscall" // Needed for fork-exec
 	"time"
 
 	"github.com/mitchellh/go-homedir"
+	"github.com/pborman/getopt"
 	"github.com/radovskyb/watcher"
 )
 
@@ -108,14 +111,25 @@ func runCmd(cmd *exec.Cmd) error {
 	return err
 }
 
-func GitYolo(messageList *[]string, r *rand.Rand) {
+func GitYolo(messageList *[]string, r *rand.Rand, respectIgnore *bool, safeMode *bool) {
 	// add gitignore ignored files
 	gitAdd := exec.Command("git", "add", ".", "--force")
-	// commit with random messag
+	if *respectIgnore || *safeMode {
+		// unless safe mode or respecting the gitignore
+		gitAdd = exec.Command("git", "add", ".")
+	}
+
+	// commit with random message
 	gitCommit := exec.Command("git", "commit", "-m", pickMessage(messageList, r))
+
 	// (force) push to master. what could possibly go wrong?
 	gitPushForce := exec.Command("git", "push", "--force", "origin", "master")
 	gitPush := exec.Command("git", "push", "--force")
+	if *safeMode {
+		// unless in safe mode...
+		gitPushForce = exec.Command("git", "push")
+		gitPush = exec.Command("git", "push")
+	}
 
 	runCmd(gitAdd)
 	runCmd(gitCommit)
@@ -123,7 +137,7 @@ func GitYolo(messageList *[]string, r *rand.Rand) {
 	runCmd(gitPush)
 }
 
-func runWatcher(messageList *[]string, r *rand.Rand) {
+func runWatcher(messageList *[]string, r *rand.Rand, respectIgnore *bool, safeMode *bool) {
 	theWatcher := watcher.New()
 	defer theWatcher.Close()
 
@@ -132,7 +146,7 @@ func runWatcher(messageList *[]string, r *rand.Rand) {
 			select {
 			case event := <-theWatcher.Event:
 				log.Println(event)
-				GitYolo(messageList, r)
+				GitYolo(messageList, r, respectIgnore, safeMode)
 			case err := <-theWatcher.Error:
 				log.Println("error:", err)
 			}
@@ -147,7 +161,37 @@ func runWatcher(messageList *[]string, r *rand.Rand) {
 }
 
 func main() {
+	optHelp := getopt.BoolLong("help", 'h', "", "Print this message and exit")
+	optSafe := getopt.BoolLong("safe", 's', "", "Safe Mode: no force push and respect `.gitignore`")
+	optRespect := getopt.BoolLong("respect", 'r', "", "Respect the `.gitignore` file")
+	optWatcher := getopt.BoolLong("watcher", 'w', "", "Watch the directory for changes, blocks STDIN ")
+	optDaemon := getopt.BoolLong("daemon", 'd', "", "Watch the directory for changes, runs in background (does not block STDIN)")
+
+	getopt.Parse()
+	if *optHelp {
+		getopt.Usage()
+		os.Exit(1)
+	}
+
 	messageList := getMessages()
 	r := rand.New(rand.NewSource(time.Now().Unix()))
-	runWatcher(&messageList, r)
+
+	if *optDaemon {
+		watchArgv := []string{}
+		watchArgv = append(watchArgv, "--watch")
+		if *optSafe {
+			watchArgv = append(watchArgv, "--safe")
+		}
+		if *optRespect {
+			watchArgv = append(watchArgv, "--respect")
+		}
+		// Fork-exec this program again as a daemon
+		// syscall.ForkExec(os.Args[0], watchArgv, nil /* TODO */)
+		log.Fatalln("DAEMON MODE NOT IMPLEMENTED")
+	} else if *optWatcher {
+		runWatcher(&messageList, r, optRespect, optSafe)
+	}
+
+	// Run-once mode, default
+	GitYolo(&messageList, r, optRespect, optSafe)
 }
